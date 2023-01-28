b364e562be39315072716bd37da4fe007248ab05
tzyuan <tzy15368@outlook.com>
2023-01-28 18:02:10 -0500

    complete p85

--- app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
@@ -2,10 +2,27 @@ package com.bignerdranch.android.geoquiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var trueBtn: Button
+    private lateinit var falseBtn: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        trueBtn = findViewById(R.id.true_button)
+        falseBtn = findViewById(R.id.false_button)
+        trueBtn.setOnClickListener { view: View ->
+            //...
+            Snackbar.make(view, "hello", Snackbar.LENGTH_LONG).show()
+
+        }
+
+        falseBtn.setOnClickListener { view: View ->
+            //...
+        }
     }
 }
