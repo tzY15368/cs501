fcddaa46650b17403724f4d9f2fce5b990c520e9
rickwayne1125 <1037457338@qq.com>
2023-02-03 17:14:17 -0500

    Use view binding

--- app/src/main/java/com/cs501/cs501app/assignment2/Calc1Activity.kt
+++ app/src/main/java/com/cs501/cs501app/assignment2/Calc1Activity.kt
@@ -10,19 +10,20 @@ import com.cs501.cs501app.utils.Alert
 
 class Calc1Activity : AppCompatActivity() {
     private lateinit var binding: ActivityCalc1Binding
+    var operators_array= arrayOf("➕","➖","✖","️➗","mod")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityCalc1Binding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        var spinner: Spinner = findViewById(R.id.spinnerOperator)
-        ArrayAdapter.createFromResource(
+        ArrayAdapter(
             this,
-            R.array.operators_array,
-            android.R.layout.simple_spinner_item
+            android.R.layout.simple_spinner_item,
+            operators_array
         ).also { adapter ->
             adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-            spinner.adapter = adapter
+            binding.spinnerOperator.adapter = adapter
         }
 
         binding.btnOp.setOnClickListener { view ->
