46d1c0325f8071bbeda0ae7cc796e8be9f968253
rickwayne1125 <1037457338@qq.com>
2023-02-03 16:38:54 -0500

    Add spinner view

--- app/src/main/java/com/cs501/cs501app/assignment2/Calc1Activity.kt
+++ app/src/main/java/com/cs501/cs501app/assignment2/Calc1Activity.kt
@@ -2,6 +2,8 @@ package com.cs501.cs501app.assignment2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import com.cs501.cs501app.R
 import com.cs501.cs501app.databinding.ActivityCalc1Binding
 import com.cs501.cs501app.utils.Alert
@@ -12,6 +14,16 @@ class Calc1Activity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityCalc1Binding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        var spinner: Spinner = findViewById(R.id.spinnerOperator)
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.operators_array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinner.adapter = adapter
+        }
 
         binding.btnOp.setOnClickListener { view ->
             Alert.success(view, "hello")
