ff3e9abd4b7fa9b954afb67e665741c6bec29f8f
M1chief <1104174361@qq.com>
2023-02-03 21:57:47 -0500

    change to binding

--- app/src/main/java/com/cs501/cs501app/assignment2/Calc2Activity.kt
+++ app/src/main/java/com/cs501/cs501app/assignment2/Calc2Activity.kt
@@ -1,6 +1,5 @@
 package com.cs501.cs501app.assignment2
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -8,106 +7,141 @@ import android.widget.Button
 import android.widget.TextView
 import com.cs501.cs501app.R
 import com.cs501.cs501app.assignment2.calFunc.cal
-import com.cs501.cs501app.databinding.ActivityCalc1Binding
-import com.cs501.cs501app.databinding.ActivityCalc2Binding
 
-class Calc2Activity : AppCompatActivity(){
-    private lateinit var binding: ActivityCalc2Binding
+class Calc2Activity : AppCompatActivity() , View.OnClickListener {
+    private var btn_0: Button? = null
+    private var btn_1: Button? = null
+    private var btn_2: Button? = null
+    private var btn_3: Button? = null
+    private var btn_4: Button? = null
+    private var btn_5: Button? = null
+    private var btn_6: Button? = null
+    private var btn_7: Button? = null
+    private var btn_8: Button? = null
+    private var btn_9: Button? = null
+    private var btn_point: Button? = null
+    private var btn_plus: Button? = null
+    private var btn_sub: Button? = null
+    private var btn_multi: Button? = null
+    private var btn_div: Button? = null
 
+    private var btn_equals: Button? = null  //等号
+    private var btn_sqrt: Button? = null  //等号
+    private var result: TextView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityCalc2Binding.inflate(layoutInflater)
-        setContentView(binding.root)
-        var str = binding.msg.text.toString();
+        setContentView(R.layout.activity_calc2)
+        initView()
+    }
 
-        binding.btn0.setOnClickListener{
-            str += "0"
-            binding.msg.text = str
-        }
+    fun initView() {
+        btn_0 = findViewById<View>(R.id.btn_0) as Button
+        btn_1 = findViewById<View>(R.id.btn_1) as Button
+        btn_2 = findViewById<View>(R.id.btn_2) as Button
+        btn_3 = findViewById<View>(R.id.btn_3) as Button
+        btn_4 = findViewById<View>(R.id.btn_4) as Button
+        btn_5 = findViewById<View>(R.id.btn_5) as Button
+        btn_6 = findViewById<View>(R.id.btn_6) as Button
+        btn_7 = findViewById<View>(R.id.btn_7) as Button
+        btn_8 = findViewById<View>(R.id.btn_8) as Button
+        btn_9 = findViewById<View>(R.id.btn_9) as Button
+        btn_point = findViewById<View>(R.id.btn_point) as Button
+        btn_div = findViewById<View>(R.id.btn_div) as Button
+        btn_plus = findViewById<View>(R.id.btn_plus) as Button
+        btn_sub = findViewById<View>(R.id.btn_sub) as Button
+        btn_multi = findViewById<View>(R.id.btn_multi) as Button
+        btn_sqrt = findViewById<View>(R.id.btn_sqrt) as Button
+        btn_equals = findViewById<View>(R.id.btn_equals) as Button
+        btn_0!!.setOnClickListener(this)
+        btn_1!!.setOnClickListener(this)
+        btn_2!!.setOnClickListener(this)
+        btn_3!!.setOnClickListener(this)
+        btn_4!!.setOnClickListener(this)
+        btn_5!!.setOnClickListener(this)
+        btn_6!!.setOnClickListener(this)
+        btn_7!!.setOnClickListener(this)
+        btn_8!!.setOnClickListener(this)
+        btn_9!!.setOnClickListener(this)
+        btn_equals!!.setOnClickListener(this)
+        btn_multi!!.setOnClickListener(this)
+        btn_sub!!.setOnClickListener(this)
+        btn_plus!!.setOnClickListener(this)
+        btn_div!!.setOnClickListener(this)
+        btn_sqrt!!.setOnClickListener(this)
+        btn_point!!.setOnClickListener(this)
+        result = findViewById<View>(R.id.msg) as TextView
+    }
+    override fun onClick(view: View) {
+        var str = result!!.text.toString()
 
-        binding.btn1.setOnClickListener{
-            str += "1"
-            binding.msg.text = str
-        }
-        binding.btn2.setOnClickListener{
-            str += "2"
-            binding.msg.text = str
-        }
-        binding.btn3.setOnClickListener{
-            str += "3"
-            binding.msg.text = str
-        }
-        binding.btn4.setOnClickListener{
-            str += "4"
-            binding.msg.text = str
-        }
-        binding.btn5.setOnClickListener{
-            str += "5"
-            binding.msg.text = str
-        }
-        binding.btn6.setOnClickListener{
-            str += "6"
-            binding.msg.text = str
-        }
-        binding.btn7.setOnClickListener{
-            str += "7"
-            binding.msg.text = str
-        }
-        binding.btn8.setOnClickListener{
-            str += "8"
-            binding.msg.text = str
-        }
-        binding.btn9.setOnClickListener{
-            str += "9"
-            binding.msg.text = str
-        }
+        when (view.id) {
+            R.id.btn_0 -> {
+                str += "0"
+                result!!.text = str
+            }
+            R.id.btn_1 -> {
+                str += "1"
+                result!!.text = str
+            }
+            R.id.btn_2 -> {
+                str += "2"
+                result!!.text = str
+            }
+            R.id.btn_3 -> {
+                str += "3"
+                result!!.text = str
+            }
+            R.id.btn_4 -> {
+                str += "4"
+                result!!.text = str
+            }
+            R.id.btn_5 -> {
+                str += "5"
+                result!!.text = str
+            }
+            R.id.btn_6 -> {
+                str += "6"
+                result!!.text = str
+            }
+            R.id.btn_7 -> {
+                str += "7"
+                result!!.text = str
+            }
+            R.id.btn_8 -> {
+                str += "8"
+                result!!.text = str
+            }
+            R.id.btn_9 -> {
+                str += "9"
+                result!!.text = str
+            }
+            R.id.btn_div -> if (str.length != 0) {
+                str += "/"
+                result!!.text = str
+            }
 
-        binding.btnDiv.setOnClickListener{
-            str += "/"
-            binding.msg.text = str
+            R.id.btn_multi -> if (str.length != 0) {
+                str += "*"
+                result!!.text = str
+            }
+            R.id.btn_point -> if (str.length != 0 && str[str.length - 1] >= '0' && str[str.length - 1] <= '9') {
+                str += "."
+                result!!.text = str
+            }
+            R.id.btn_equals -> if (str.length != 0) {
+                str += "+"
+                result!!.text = str
+            }
+            R.id.btn_sub -> if (str.length != 0) {
+                str += "-"
+                result!!.text = str
+            }
+            R.id.btn_sqrt -> if (str.length != 0) {
+                str += "√"
+                result!!.text = str
+            }
+            R.id.btn_equals ->
+                result!!.text = calFunc.calc(cal(str))//输入结束，转为逆波兰表达式
         }
-
-        binding.btnMulti.setOnClickListener{
-            str += "*"
-            binding.msg.text = str
-        }
-        binding.btnPlus.setOnClickListener{
-            str += "+"
-            binding.msg.text = str
-        }
-        binding.btnSub.setOnClickListener{
-            str += "-"
-            binding.msg.text = str
-        }
-        binding.btnSqrt.setOnClickListener{
-            str += "√"
-            binding.msg.text = str
-        }
-
-        binding.btnMod.setOnClickListener{
-            str += "%"
-            binding.msg.text = str
-        }
-
-        binding.btnPoint.setOnClickListener{
-            str += "."
-            binding.msg.text = str
-        }
-
-        binding.btnEquals.setOnClickListener{
-
-            binding.msg.text = calFunc.calc(cal(str))//输入结束，转为逆波兰表达式
-        }
-
-        binding.btnac.setOnClickListener{
-            str = ""
-            binding.msg.text = str
-        }
-
-        binding.btndel.setOnClickListener{
-            str = str.substring(0, str.length - 1)
-            binding.msg.text = str
-        }
-
     }
 }
