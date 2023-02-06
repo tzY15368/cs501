a0b68fb8c2d75d8d08ecc958a9911237a09645d1
Yingzhe Dong <51332815+sdsz20142087@users.noreply.github.com>
2023-02-07 04:04:07 +0800

    insert EditText, support mixed keyboard and button input, according to 5 requirement (#24)
    * calc2_edit_text
    
    * update editText
    
    * merget ac
    
    * update ui size

--- app/src/main/java/com/cs501/cs501app/assignment2/Calc2Activity.kt
+++ app/src/main/java/com/cs501/cs501app/assignment2/Calc2Activity.kt
@@ -2,6 +2,7 @@ package com.cs501.cs501app.assignment2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.InputType
 import com.cs501.cs501app.databinding.ActivityCalc2Binding
 import com.cs501.cs501app.utils.Alert
 
@@ -9,98 +10,86 @@ class Calc2Activity : AppCompatActivity() {
     private lateinit var binding: ActivityCalc2Binding
     // operateCount should be less than digitCount to make sure expression valid
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         binding = ActivityCalc2Binding.inflate(layoutInflater)
         setContentView(binding.root)
-        var str = binding.msg.text.toString();
+        var str = "" //binding.msg.text.toString();
+
+        binding.editAdvanced?.inputType = InputType.TYPE_NULL
 
         binding.btn0.setOnClickListener {
-            str += "0"
-            binding.msg.text = str
+            updateSettings("0")
         }
 
         binding.btn1.setOnClickListener {
-            str += "1"
-            binding.msg.text = str
+            updateSettings("1")
         }
         binding.btn2.setOnClickListener {
-            str += "2"
-            binding.msg.text = str
+            updateSettings("2")
         }
         binding.btn3.setOnClickListener {
-            str += "3"
-            binding.msg.text = str
+            updateSettings("3")
         }
         binding.btn4.setOnClickListener {
-            str += "4"
-            binding.msg.text = str
+            updateSettings("4")
         }
         binding.btn5.setOnClickListener {
-            str += "5"
-            binding.msg.text = str
+            updateSettings("5")
         }
         binding.btn6.setOnClickListener {
-            str += "6"
-            binding.msg.text = str
+            updateSettings("6")
         }
         binding.btn7.setOnClickListener {
-            str += "7"
-            binding.msg.text = str
+            updateSettings("7")
         }
         binding.btn8.setOnClickListener {
-            str += "8"
-            binding.msg.text = str
+            updateSettings("8")
         }
         binding.btn9.setOnClickListener {
-            str += "9"
-            binding.msg.text = str
+            updateSettings("9")
         }
 
         binding.btnDiv.setOnClickListener {
-            str += "/"
-            binding.msg.text = str
+            updateSettings("/")
         }
 
         binding.btnMulti.setOnClickListener {
-            str += "*"
-            binding.msg.text = str
+            updateSettings("*")
         }
         binding.btnPlus.setOnClickListener {
-            str += "+"
-            binding.msg.text = str
+            updateSettings("+")
         }
         binding.btnSub.setOnClickListener {
-            str += "-"
-            binding.msg.text = str
+            updateSettings("-")
         }
         binding.btnSqrt.setOnClickListener {
-            str += "√"
-            binding.msg.text = str
+            updateSettings("√")
         }
 
         binding.btnPoint.setOnClickListener {
-            str += "."
-            binding.msg.text = str
+            updateSettings(".")
         }
 
         binding.btnMod.setOnClickListener{
-            str += "%"
-            binding.msg.text = str
+            updateSettings("%")
         }
         binding.btnDel.setOnClickListener{
+            str = binding.editAdvanced.text.toString()
             str = str.substring(0, str.length - 1)
-            binding.msg.text = str
+            binding.editAdvanced.setText(str)
+            binding.editAdvanced.setSelection(binding.editAdvanced.length())
         }
         binding.btnAc.setOnClickListener{
             str = ""
-            binding.msg.text = str
+            binding.editAdvanced.setText(str)
+            binding.editAdvanced.setSelection(binding.editAdvanced.length())
         }
 
         binding.btnEquals.setOnClickListener { view ->
             var resultMsg = ""
+            str = binding.editAdvanced.text.toString()
             try {
                 resultMsg = calFunc.evalExpr(str)
                 Alert.success(view, resultMsg)
@@ -108,9 +97,17 @@ class Calc2Activity : AppCompatActivity() {
                 resultMsg = "ERROR:$e"
                 Alert.fail(view, e.toString())
             }
-            binding.msg.text = resultMsg//输入结束，转为逆波兰表达式
-
+//            binding.msg.text = resultMsg // change format
+            binding.editAdvanced.setText(resultMsg)
         }
+    }
+
+    private fun updateSettings(add_str: String) {
+        var temp = binding.editAdvanced.text.toString()
+        temp += add_str
+//        binding.msg.text = temp
+        binding.editAdvanced.setText(temp)
+        binding.editAdvanced.setSelection(binding.editAdvanced.length())
     }
 
 }
