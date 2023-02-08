1bf22f2f0da133ac064be6fda191f353a9a7d19d
tzyuan <tzy15368@outlook.com>
2023-02-08 00:27:06 -0500

    add scaffold for assignment3

--- app/src/main/java/com/cs501/cs501app/MainActivity.kt
+++ app/src/main/java/com/cs501/cs501app/MainActivity.kt
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.cs501.cs501app.assignment2.Calc1Activity
 import com.cs501.cs501app.assignment2.Calc2Activity
+import com.cs501.cs501app.assignment3.flashcard.FCLoginActivity
+import com.cs501.cs501app.assignment3.geoquiz.GeoQuizActivity
+import com.cs501.cs501app.assignment3.tempconv.TempConvActivity
 import com.cs501.cs501app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -21,6 +24,18 @@ class MainActivity : AppCompatActivity() {
         }
         binding.button22.setOnClickListener {
             val intent = Intent(this, Calc2Activity::class.java)
+            startActivity(intent)
+        }
+        binding.button31.setOnClickListener {
+            val intent = Intent(this, GeoQuizActivity::class.java)
+            startActivity(intent)
+        }
+        binding.button32.setOnClickListener {
+            val intent = Intent(this, TempConvActivity::class.java)
+            startActivity(intent)
+        }
+        binding.button33.setOnClickListener {
+            val intent = Intent(this, FCLoginActivity::class.java)
             startActivity(intent)
         }
     }
---
+++ app/src/main/java/com/cs501/cs501app/assignment3/flashcard/FCHome.kt
@@ -0,0 +1,12 @@
+package com.cs501.cs501app.assignment3.flashcard
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.cs501.cs501app.R
+
+class FCHome : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fchome)
+    }
+}
