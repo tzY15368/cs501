9d085ed0fe418064e3abeb3cb36cebc3711d9063
tzyuan <tzy15368@outlook.com>
2023-02-08 18:10:12 -0500

    improve code structure

--- app/src/main/java/com/cs501/cs501app/assignment3/tempconv/TempConvActivity.kt
+++ app/src/main/java/com/cs501/cs501app/assignment3/tempconv/TempConvActivity.kt
@@ -6,53 +6,67 @@ import android.widget.SeekBar
 import android.widget.SeekBar.OnSeekBarChangeListener
 import androidx.appcompat.app.AppCompatActivity
 import com.cs501.cs501app.databinding.ActivityTempConvBinding
+
 class TempConvActivity : AppCompatActivity() {
     private lateinit var binding: ActivityTempConvBinding;
-    private var tempC = 0.0
-    private var tempF = 0.0
+    private var tempC = 0
+    private var tempF = 32
+    private var strChangeThres = 20
+    private val STR_WARMER = "I wish it was warmer."
+    private val STR_COLDER = "I wish it was colder."
+
+    private fun updateText(temp: Int, isTempC: Boolean) {
+        Log.d("TempConvActivity", "updateText: $temp, $isTempC")
+        if (isTempC) {
+            tempC = temp
+            tempF = ((tempC * 1.8 + 32).toInt())
+        } else {
+            tempF = temp
+            tempC = (((tempF - 32) * .5556).toInt())
+        }
+        Log.d("tempC", tempC.toString())
+        binding.seekBarC.progress = tempC
+        binding.seekBarF.progress = tempF
+        binding.textViewTempC.text = tempC.toString()
+        binding.textViewTempF.text = tempF.toString()
+        if (tempC < strChangeThres) {
+            binding.textViewIntMsg.text = STR_WARMER
+        } else {
+            binding.textViewIntMsg.text = STR_COLDER
+        }
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityTempConvBinding.inflate(layoutInflater)
+        binding.seekBarC.incrementProgressBy(1)
+        binding.seekBarF.incrementProgressBy(1)
         setContentView(binding.root)
-        binding.seekBarC.setOnSeekBarChangeListener(object : OnSeekBarChangeListener {
+        updateText(0, true)
+        class MyOnseekBarChangeListener(// constructor with one argument of type int
+            private var tempLowerThres: Int, private var isTempC: Boolean
+        ) : OnSeekBarChangeListener {
+            private var hasInput = false
             override fun onProgressChanged(seekBar: SeekBar, tempC: Int, able: Boolean) {
-                tempF = (Integer.valueOf(tempC.toString()) * 1.8 + 32)
-                binding.textViewTempC.text = tempC.toString()
-                binding.textViewTempF.text = tempF.toString()
-                //Log.e("temp",tempC.toString())
-                if (tempC < 20) {
-                    binding.textViewIntMsg.text = "I wish it were warmer."
-                } else {
-                    binding.textViewIntMsg.text = "I wish it were colder."
+                if (!hasInput) return
+                if (tempC < tempLowerThres) {
+                    seekBar.progress = tempLowerThres
+                    return
                 }
+                updateText(tempC, isTempC)
             }
 
-            override fun onStartTrackingTouch(seekBar: SeekBar) {}
-            override fun onStopTrackingTouch(seekBar: SeekBar) {
-                binding.seekBarF.progress = tempF.toInt()
-            }
-        })
-        binding.seekBarF.setOnSeekBarChangeListener(object : OnSeekBarChangeListener {
-            override fun onProgressChanged(seekBar: SeekBar, tempF: Int, able: Boolean) {
-                tempC = ((Integer.valueOf(tempF.toString()) - 32) * .5556)
-                binding.textViewTempF.text = tempF.toString()
-                binding.textViewTempC.text = tempC.toString()
-                if (tempC < 20) {
-                    binding.textViewIntMsg.text = "I wish it were warmer."
-                } else {
-                    binding.textViewIntMsg.text = "I wish it were colder."
-                }
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+                hasInput = true
             }
 
-            override fun onStartTrackingTouch(seekBar: SeekBar) {}
-            override fun onStopTrackingTouch(seekBar: SeekBar) {
-                binding.seekBarC.progress = tempC.toInt()
-                if (tempF < 20) {
-                    binding.textViewIntMsg.text = "I wish it were warmer."
-                } else {
-                    binding.textViewIntMsg.text = "I wish it were colder."
-                }
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+                hasInput = false
             }
-        })
+        }
+        binding.seekBarC.setOnSeekBarChangeListener(MyOnseekBarChangeListener(0, true))
+        binding.seekBarF.setOnSeekBarChangeListener(MyOnseekBarChangeListener(32, false))
     }
 }
+
