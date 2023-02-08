62dea12900c95a5334a07051afe8176fe7c349fa
tzyuan <tzy15368@outlook.com>
2023-02-08 01:21:09 -0500

    add jetpack compose to project

--- app/src/main/java/com/cs501/cs501app/assignment3/tempconv/TempConvActivity.kt
+++ app/src/main/java/com/cs501/cs501app/assignment3/tempconv/TempConvActivity.kt
@@ -1,12 +1,43 @@
 package com.cs501.cs501app.assignment3.tempconv
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.cs501.cs501app.R
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 
-class TempConvActivity : AppCompatActivity() {
+class TempConvActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_temp_conv)
+        setContent {
+            MessageCard(Message("好变态啊", "Jetpack Compose"))
+        }
     }
+
+}
+
+data class Message(val author: String, val body: String)
+
+@Composable
+fun MessageCard(msg: Message) {
+    Row() {
+        Text(text= msg.author)
+
+        Column() {
+            Text(text = msg.author)
+            Text(text = msg.body)
+        }
+    }
+
+}
+
+@Preview
+@Composable
+fun PreviewMessageCard() {
+    MessageCard(
+        msg = Message("Colleague", "Hey, take a look at Jetpack Compose, it's great!")
+    )
 }
