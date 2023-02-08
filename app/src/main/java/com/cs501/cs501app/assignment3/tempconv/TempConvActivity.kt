648d73e887f4ac27f5240cd3bc54183c9b564a73
tzyuan <tzy15368@outlook.com>
2023-02-08 00:27:06 -0500

    add scaffold for assignment3

---
+++ app/src/main/java/com/cs501/cs501app/assignment3/tempconv/TempConvActivity.kt
@@ -0,0 +1,12 @@
+package com.cs501.cs501app.assignment3.tempconv
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.cs501.cs501app.R
+
+class TempConvActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_temp_conv)
+    }
+}
