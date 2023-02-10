a0493dbc9ea4a625b671c55acf4d3e3443213376
Yingzhe Dong <yzdong99@bu.edu>
2023-02-10 16:32:12 -0500

    finish_backend

---
+++ app/src/test/java/com/cs501/cs501app/assignment3/flashcard/FCBackendTest.kt
@@ -0,0 +1,16 @@
+package com.cs501.cs501app.assignment3.flashcard
+
+import org.junit.Assert.*
+
+import org.junit.Test
+
+class FCBackendTest {
+
+    @Test
+    fun getCurrentProblem() {
+    }
+
+    @Test
+    fun checkAnswer() {
+    }
+}
