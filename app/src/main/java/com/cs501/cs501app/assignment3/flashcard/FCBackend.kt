0166078a7b7fbf5a6e041deeb4bc0810be6d4d89
Yingzhe Dong <yzdong99@bu.edu>
2023-02-10 17:29:11 -0500

    update FCBackend

--- app/src/main/java/com/cs501/cs501app/assignment3/flashcard/FCBackend.kt
+++ app/src/main/java/com/cs501/cs501app/assignment3/flashcard/FCBackend.kt
@@ -8,9 +8,9 @@ import java.util.*
 const val CURRENT_INDEX_KEY = "CURRENT_INDEX_KEY"
 const val CURRENT_SCORE_KEY = "CURRENT_SCORE_KEY"
 
-class FCBackend(private val savedStateHandle: SavedStateHandle)  : ViewModel() {
-    private val problemList =  mutableListOf<FCProblem>()
-    private val answerList = mutableListOf<Float>()
+class FCBackend(private val savedStateHandle: SavedStateHandle) : ViewModel() {
+    val problemList = mutableListOf<FCProblem>()
+    val answerList = mutableListOf<Float>()
     private val rangeOp1Start = 1.0f
     private val rangeOp1End = 99.0f
     private val rangeOp2Start = 1.0f
@@ -27,24 +27,42 @@ class FCBackend(private val savedStateHandle: SavedStateHandle)  : ViewModel() {
         get() = savedStateHandle.get(CURRENT_SCORE_KEY) ?: 0
         set(value) = savedStateHandle.set(CURRENT_SCORE_KEY, value)
 
-    fun getCurrentProblem() : FCProblem {
+    /**
+     * get current problem, it will automatically jump to next problem for next call.
+     *
+     * @return FCProblem object.
+     */
+    fun getCurrentProblem(): FCProblem {
         return problemList.get(current_index)
     }
 
-    fun checkAnswer(targetAnswer : String) : Boolean {
+    /**
+     * check if the user's answer is correct.
+     *
+     * @param targetAnswer user's answer.
+     * @return if the answer is correct.
+     */
+
+    fun checkAnswer(targetAnswer: String): Boolean {
         val expectAnswer = answerList.get(current_index).toString()
+        val targetAnswer = BigDecimal(targetAnswer).stripTrailingZeros().setScale(1).toString()
         val isCorrect = BigDecimal(expectAnswer) == BigDecimal(targetAnswer)
         current_index++
         if (isCorrect) current_score++
         return isCorrect
     }
 
+    /**
+     * generate all problems for each game, call this function if user want to begin a new game (don't need to clear state, it will call automatically)
+     *
+     * @return a list of problem, each type is FCProblem object
+     */
     fun generateTenProblems(): MutableList<FCProblem> {
         clearAllList()
         for (i in 0 until 10) {
             val genereatedProblem = generateOneProblem(i)
             problemList.add(i, genereatedProblem)
-            val answer : Float = if (genereatedProblem.isPlus) {
+            val answer: Float = if (genereatedProblem.isPlus) {
                 (BigDecimal(genereatedProblem.operand1) + BigDecimal(genereatedProblem.operand2)).toFloat()
             } else {
                 (BigDecimal(genereatedProblem.operand1) - BigDecimal(genereatedProblem.operand2)).toFloat()
@@ -54,7 +72,13 @@ class FCBackend(private val savedStateHandle: SavedStateHandle)  : ViewModel() {
         return problemList
     }
 
-    private fun generateOneProblem(index : Int) : FCProblem {
+    /**
+     * inner function to support generateTenProblems()
+     *
+     * @param index problem number, from 0 - 9
+     * @return a FCProblem object
+     */
+    private fun generateOneProblem(index: Int): FCProblem {
         val randomFloat1 = random.nextFloat() * (rangeOp1End - rangeOp1Start) + rangeOp1Start
         val formattedFloat1 = format.format(randomFloat1)
         val op1 = formattedFloat1.toFloat().toString()
@@ -73,14 +97,27 @@ class FCBackend(private val savedStateHandle: SavedStateHandle)  : ViewModel() {
         return problem
     }
 
-    fun getCurrentIndex() : Int {
+    /**
+     * get current problem index, from 0 - 9
+     *
+     * @return current problem index
+     */
+    fun getCurrentIndex(): Int {
         return current_index
     }
 
-    fun getCurrentScore() : Int {
+    /**
+     * get current score for the game, from 0 - 10
+     *
+     * @return current game score
+     */
+    fun getCurrentScore(): Int {
         return current_score
     }
 
+    /**
+     * reset state for each game, call automatically from generateTenProblems()
+     */
     private fun clearAllList() {
         problemList.clear()
         answerList.clear()
