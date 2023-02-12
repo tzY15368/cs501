f79fcff93a14504ccb2b4796046c307a3d307546
Jace <114544588+Jace9999@users.noreply.github.com>
2023-02-12 13:28:59 -0500

    Update A3 geoquiz (#59)

---
+++ app/src/main/java/com/cs501/cs501app/assignment3/geoquiz/CheatActivity.kt
@@ -0,0 +1,98 @@
+package com.cs501.cs501app.assignment3.geoquiz
+
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.viewModels
+import com.cs501.cs501app.R
+import com.cs501.cs501app.databinding.ActivityCheatBinding
+
+const val EXTRA_ANSWER_SHOWN = "com.example.android.geoquiz.answer_shown"
+private const val TAG = "CheatActivity"
+private const val EXTRA_ANSWER_IS_TRUE =
+    "com.example.geoquiz.answer_is_true"
+
+private var answerIsTrue = false
+
+private lateinit var answerTextView: TextView
+private lateinit var showAnswerButton: Button
+
+
+
+class CheatActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityCheatBinding
+
+    private val cheatViewModel: CheatViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityCheatBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        answerIsTrue = intent.getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false)
+
+        answerTextView = findViewById(R.id.answer_text_view)
+        showAnswerButton = findViewById(R.id.show_answer_button)
+
+        binding.showAnswerButton.setOnClickListener {
+            val answerText = when {
+                answerIsTrue -> R.string.true_button
+                else -> R.string.false_button
+            }
+            binding.answerTextView.setText(answerText)
+            setAnswerShownResult(true)
+            cheatViewModel.answerWasRevealed = true
+            cheatViewModel.recordForCurAnswer = answerIsTrue
+        }
+
+        if (cheatViewModel.answerWasRevealed) {
+            val answerText = when {
+                cheatViewModel.recordForCurAnswer -> R.string.true_button
+                else -> R.string.false_button
+            }
+            answerTextView.setText(answerText)
+            setAnswerShownResult(true)
+        }
+    }
+
+    private fun setAnswerShownResult(isAnswerShown: Boolean) {
+        val data = Intent().apply {
+            putExtra(EXTRA_ANSWER_SHOWN, isAnswerShown)
+        }
+        setResult(Activity.RESULT_OK, data)
+    }
+
+    companion object {
+        fun newIntent(packageContext: Context, answerIsTrue: Boolean): Intent {
+            return Intent(packageContext, CheatActivity::class.java).apply {
+                putExtra(EXTRA_ANSWER_IS_TRUE, answerIsTrue)
+            }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart() called")
+    }
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume() called")
+    }
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause() called")
+    }
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop() called")
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy() called")
+    }
+}
