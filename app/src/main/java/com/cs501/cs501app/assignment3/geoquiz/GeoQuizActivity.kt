ccbfbc0077665d203aac4ccd74f9aa8ad849fcf0
Jace9999 <jacezhu99@gmail.com>
2023-02-12 13:20:43 -0500

    Update A3 geoquiz

--- app/src/main/java/com/cs501/cs501app/assignment3/geoquiz/GeoQuizActivity.kt
+++ app/src/main/java/com/cs501/cs501app/assignment3/geoquiz/GeoQuizActivity.kt
@@ -1,12 +1,105 @@
 package com.cs501.cs501app.assignment3.geoquiz
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.viewModels
 import com.cs501.cs501app.R
+import com.cs501.cs501app.databinding.ActivityGeoQuizBinding
 
+private const val TAG = "MainActivity"
 class GeoQuizActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityGeoQuizBinding
+
+    private val quizViewModel: QuizViewModel by viewModels()
+
+    private val cheatLauncher = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            quizViewModel.curCheated()
+//        quizViewModel.isCheater =
+//            result.data?.getBooleanExtra(EXTRA_ANSWER_SHOWN, false) ?: false
+        }
+
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_geo_quiz)
+        Log.d(TAG, "onCreate(Bundle?) called")
+
+        binding = ActivityGeoQuizBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        Log.d(TAG, "Got a QuizViewModel: $quizViewModel")
+
+        binding.trueButton.setOnClickListener { view: View ->
+            checkAnswer(true)
+        }
+        binding.falseButton.setOnClickListener { view: View ->
+            checkAnswer(false)
+        }
+
+        binding.nextButton.setOnClickListener {
+            quizViewModel.moveToNext()
+            updateQuestion()
+        }
+        binding.questionTextView.setOnClickListener{
+            quizViewModel.moveToNext()
+            updateQuestion()
+        }
+        binding.previousButton.setOnClickListener {
+            quizViewModel.moveToPrevious()
+            updateQuestion()
+        }
+        binding.cheatButton.setOnClickListener {
+            val answerIsTrue = quizViewModel.currentQuestionAnswer
+            val intent = CheatActivity.newIntent(this@GeoQuizActivity, answerIsTrue)
+            cheatLauncher.launch(intent)
+        }
+        updateQuestion()
+    }
+
+    private fun updateQuestion() {
+        val questionTextResId = quizViewModel.currentQuestionText
+        binding.questionTextView.setText(questionTextResId)
+    }
+
+    private fun checkAnswer(userAnswer: Boolean) {
+        val correctAnswer = quizViewModel.currentQuestionAnswer
+        val messageResId = when {
+            quizViewModel.checkCurCheat() -> R.string.judgment_toast
+            userAnswer == correctAnswer -> R.string.correct_toast
+            else -> R.string.incorrect_toast
+        }
+        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT)
+            .show()
+    }
+
+
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart() called")
+    }
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume() called")
+    }
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause() called")
+    }
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop() called")
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy() called")
     }
 }
