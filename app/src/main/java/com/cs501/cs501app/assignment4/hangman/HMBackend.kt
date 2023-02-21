a9686f34c9cc006895a7f8153d10dc0198d9c4da
M13ch1ef <59638565+M1schief@users.noreply.github.com>
2023-02-21 18:32:16 -0500

    Zhaohc (#69)
    hangman backend

--- app/src/main/java/com/cs501/cs501app/assignment4/hangman/HMBackend.kt
+++ app/src/main/java/com/cs501/cs501app/assignment4/hangman/HMBackend.kt
@@ -1,9 +1,22 @@
 package com.cs501.cs501app.assignment4.hangman
 
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 
 class HMBackend(private val savedStateHandle: SavedStateHandle): ViewModel() {
+    var randomWordArray = charArrayOf() // word selected put into char array
+    private var hangmanBody: Array<ImageView?> = Array(6) { null }
+    var countWrong = 0 // number wrong
+    var wordSelectedUnderscoresArr =
+        java.util.ArrayList<Char>() // displayed string of underscores arraylist
+    private var wordSelected // word selected textview
+            : TextView? = null
+    var counthint = 0
+    var category: String? = null
     // returns the hint for the current word, this could change state in the backend
 //    fun getHint(): String
 //
@@ -21,4 +34,83 @@ class HMBackend(private val savedStateHandle: SavedStateHandle): ViewModel() {
 //
 //    // returns if the player has won
 //    fun hasWon(): Boolean
+
+    public fun gameEnd(): Boolean{
+        if (countWrong == hangmanBody.size) {
+           return true;
+        }
+        return false;
+    }
+
+    public fun hasWon(): Boolean{
+        // whole word completed
+        var contains = false
+        for (c in wordSelectedUnderscoresArr) {
+            if (c == '_') {
+                contains = true
+                break
+            }
+        }
+        return !contains;
+    }
+
+    public fun getHint():String{
+        counthint++
+        if (counthint < 2) {
+           return category.toString()
+        }
+        return "no more hint"
+
+    }
+
+    public fun getHP(): Int{
+        return 6 - countWrong
+    }
+
+
+
+    public fun letterChecker(fromButton: Char) {
+        // linear search to check if input letter is in the word
+        var check = false
+        for (c in randomWordArray) {
+            if (c == fromButton) {
+                check = true
+            }
+        }
+        val indexList: MutableList<Int> = ArrayList()
+        if (!check) {
+            // negative value means not in the word -> wrong -> set next body part visible
+
+            //set body part visible
+            hangmanBody[countWrong]!!.visibility = View.VISIBLE
+
+            countWrong++
+
+            if(gameEnd()){
+                //todo: make toast
+            }
+        } else {
+            // else user got it right -> update underscore(s)
+
+            // getting the array indexes of letter instance in word
+            for (iter in randomWordArray.indices) {
+                if (randomWordArray.get(iter) == fromButton) {
+                    indexList.add(iter)
+                }
+            }
+
+            // updating the array of underscores
+            for (iter in indexList.indices) {
+                wordSelectedUnderscoresArr.set(indexList[iter], fromButton)
+            }
+            val sb = StringBuilder()
+            for (ch in wordSelectedUnderscoresArr) {
+                sb.append(ch)
+            }
+            // TODO: set words selected
+
+            // whole word completed
+            hasWon()
+        }
+    }
 }
