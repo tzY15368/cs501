5a4d446f571ea04e498b5274944cdb1bf9964410
tzyuan <tzy15368@outlook.com>
2023-05-04 13:53:53 -0400

    impl user type edit

--- app/src/main/java/com/cs501/cs501app/buotg/view/user_setting/UserInfoEdit.kt
+++ app/src/main/java/com/cs501/cs501app/buotg/view/user_setting/UserInfoEdit.kt
@@ -3,14 +3,12 @@ package com.cs501.cs501app.buotg.view.user_setting
 import android.content.Context
 import android.widget.Toast
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.cs501.cs501app.R
 import com.cs501.cs501app.buotg.CustomButton
@@ -18,7 +16,6 @@ import com.cs501.cs501app.buotg.CustomTextField
 import com.cs501.cs501app.buotg.database.entities.User
 import com.cs501.cs501app.buotg.database.entities.UserType
 import com.cs501.cs501app.buotg.database.repositories.AppRepository
-import com.cs501.cs501app.utils.GenericTopAppBar
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
@@ -29,6 +26,7 @@ import java.util.*
 fun UserInfoEdit(done: () -> Unit = {}, context: Context) {
     val userRepo = AppRepository.get().userRepo()
     val coroutineScope = rememberCoroutineScope()
+    val ctx = LocalContext.current
     val currentUser = remember { mutableStateOf<User?>(null) }
 //    val currentUser = User(
 //        UUID.randomUUID(),
@@ -60,7 +58,7 @@ fun UserInfoEdit(done: () -> Unit = {}, context: Context) {
                     // Update the user info in the database
                     coroutineScope.launch {
                         withContext(Dispatchers.IO) {
-                            userRepo.updateUser(updatedUser)
+                            userRepo.updateUser(ctx, updatedUser)
                         }
                         Toast.makeText(context, text, Toast.LENGTH_SHORT).show()
                         done()
@@ -75,26 +73,72 @@ fun UserInfoEdit(done: () -> Unit = {}, context: Context) {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun UserInfoForm(user: User, onUserInfoUpdated: (User) -> Unit) {
-    val full_name = remember { mutableStateOf(user.full_name) }
-    val email = remember { mutableStateOf(user.email) }
-
+    val thisUser = user.copy()
+    val full_name = remember { mutableStateOf(thisUser.full_name) }
+    val email = remember { mutableStateOf(thisUser.email) }
+    var typeMenuExpanded by remember { mutableStateOf(false) }
+    var selectedType by remember { mutableStateOf(thisUser.user_type) }
     Column(
         verticalArrangement = Arrangement.spacedBy(16.dp),
         modifier = Modifier.fillMaxWidth()
     ) {
-        CustomTextField(value = full_name.value, onValueChange = { full_name.value = it }, stringResource(id = R.string.name) )
-        CustomTextField(value = email.value, onValueChange = { email.value = it }, stringResource(id = R.string.email) )
-        CustomButton(onClick = {
-            // Create a new user object with the updated info
-            val updatedUser = User(
-                created_at = user.created_at,
-                full_name = full_name.value,
-                email = email.value,
-                user_type = user.user_type,
-                user_id = user.user_id
+        Row {
+            CustomTextField(
+                value = full_name.value,
+                onValueChange = { full_name.value = it },
+                stringResource(id = R.string.name)
             )
-            // Call the onUserInfoUpdated callback to notify the caller of the updated user info
-            onUserInfoUpdated(updatedUser)
-        }, text = stringResource(id = R.string.save_2))
+        }
+        Row {
+            CustomTextField(
+                value = email.value,
+                onValueChange = { email.value = it },
+                stringResource(id = R.string.email)
+            )
+        }
+        Row {
+            ExposedDropdownMenuBox(
+                expanded = typeMenuExpanded,
+                onExpandedChange = { typeMenuExpanded = !typeMenuExpanded }) {
+                TextField(
+                    value = selectedType.toString(),
+                    onValueChange = {},
+                    readOnly = true,
+                    trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = typeMenuExpanded) },
+                    modifier = Modifier.menuAnchor()
+                )
+                ExposedDropdownMenu(
+                    expanded = typeMenuExpanded,
+                    onDismissRequest = { typeMenuExpanded = false }) {
+                    DropdownMenuItem(
+                        text = { Text(stringResource(id = R.string.type_student)) },
+                        onClick = {
+                            typeMenuExpanded = false
+                            selectedType = UserType.student
+                        })
+                    Divider()
+                    DropdownMenuItem(
+                        text = { Text(stringResource(id = R.string.type_teacher)) },
+                        onClick = {
+                            typeMenuExpanded = false
+                            selectedType = UserType.teacher
+                        })
+                }
+            }
+        }
+        Row {
+            CustomButton(onClick = {
+                // Create a new user object with the updated info
+                val updatedUser = User(
+                    created_at = thisUser.created_at,
+                    full_name = full_name.value,
+                    email = email.value,
+                    user_type = selectedType,
+                    user_id = thisUser.user_id
+                )
+                // Call the onUserInfoUpdated callback to notify the caller of the updated user info
+                onUserInfoUpdated(updatedUser)
+            }, text = stringResource(id = R.string.save_2))
+        }
     }
 }
